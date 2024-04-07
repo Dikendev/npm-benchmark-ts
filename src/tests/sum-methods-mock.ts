a81f041052b55c89c7f7e580f94c4c36164cf7b0
Dikendev <diken.dev@gmail.com>
2024-04-07 20:07:28 -0300

    change moved  tests mock file

--- src/tests/sum-methods-mock.ts
+++ src/tests/sum-methods-mock.ts
@@ -11,3 +11,11 @@ export class SumMethods {
 		return numbers.reduce((acc, current) => acc + current, 0);
 	}
 }
+
+export const generateArray = (length: number): number[] => {
+	const numbers: number[] = [];
+	for (let i = 0; i < length; i++) {
+		numbers.push(i);
+	}
+	return numbers;
+};
--- src/utils/generate-numbers.ts
+++
@@ -1,7 +0,0 @@
-export const generateArray = (length: number): number[] => {
-	const numbers: number[] = [];
-	for (let i = 0; i < length; i++) {
-		numbers.push(i);
-	}
-	return numbers;
-};
