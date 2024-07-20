78c191e65d89200bb0b4cbfe602602fbd2b958cd
Dikendev <diken.dev@gmail.com>
2024-07-19 23:47:07 -0300

    add file type and file options

--- src/utils/file-path.ts
+++ src/utils/file-path.ts
@@ -1,10 +1,27 @@
 import path from "path";
 
-export const filePath = (dirPath: string, fileName: string): string => {
+export const FileType = {
+	JSON: "json",
+	PNG: "png",
+} as const;
+
+export type FileType = (typeof FileType)[keyof typeof FileType];
+
+export interface FileOptions {
+	dirPath: string;
+	fileName: string;
+	fileType: FileType;
+}
+
+/**
+ * @description Get the file path
+ */
+export const filePath = (fileOptions: FileOptions): string => {
 	try {
+		const { dirPath, fileName, fileType } = fileOptions;
 		if (!dirPath) throw new Error("Directory path is required");
 		if (!fileName) throw new Error("File name is required");
-		return path.join(dirPath, fileName);
+		return path.join(dirPath, `${fileName}.${fileType}`);
 	} catch (error) {
 		throw error;
 	}
