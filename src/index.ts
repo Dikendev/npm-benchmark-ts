dd1e547796f04f34ff3cd5075d5bc4216b4ecc11
Dikendev <diken.dev@gmail.com>
2024-07-19 23:52:35 -0300

    refactor: update param descriptions and function order

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Options, saveJsonFile } from "./utils/save-json-file";
-import { ChartData } from "./utils/save-chart-data";
+import { ChartData } from "./chart/chart-data";
 
 export interface ChartDataResult {
 	[key: string]: ChartDataResultBenchmark;
@@ -13,56 +13,61 @@ export interface ChartDataResultBenchmark {
 export type FunctionUnderBenchmark<T, U> = (...args: T[]) => U;
 
 export interface BenchmarkFunctions<T, U> {
-	functionDescription: string;
+	name: string;
 	functionUnderTest: FunctionUnderBenchmark<T, U>;
-	detail?: string;
+	description?: string;
 }
 
-/**
- * @description Measure the performance of the functions, lower time is better
- */
-export function measurePerformance<T, U>(
-	benchmarkFunctions: BenchmarkFunctions<T, U>[]
-): ChartDataResult {
-	const chartData: ChartDataResult = {};
-	for (const {
-		functionDescription,
-		functionUnderTest,
-		detail,
-	} of benchmarkFunctions) {
-		performance.mark("start");
-		functionUnderTest();
-		performance.mark("end");
-		performance.measure(functionDescription, {
-			start: "start",
-			end: "end",
-			detail,
-		});
-		const measureResults = performance.getEntriesByName(functionDescription);
-		measureResults.forEach((measureResult) => {
-			chartData[functionDescription] = {
-				name: measureResult.name,
-				duration: measureResult.duration,
-			};
-		});
-	}
-	return chartData;
-}
+export class BenchMarkJs {
+	chart: ChartData;
 
-/**
- * @description Benchmark function and create a chart
- */
-export async function benchMark<T, U>(
-	benchMarkNameFile: string,
-	benchmarkFunctions: BenchmarkFunctions<T, U>[],
-	options?: Options
-): Promise<ChartDataResult> {
-	const chartData = measurePerformance(benchmarkFunctions);
-
-	if (options?.dirPath !== undefined) {
-		ChartData.createChart(benchMarkNameFile, chartData, options);
-		await saveJsonFile(benchMarkNameFile, chartData, options);
+	constructor() {
+		this.chart = new ChartData();
 	}
 
-	return chartData;
+	/**
+	 * @description Benchmark function and create a chart
+	 */
+	async benchMark<T, U>(
+		benchMarkNameFile: string,
+		benchmarkFunctions: BenchmarkFunctions<T, U>[],
+		options?: Options
+	): Promise<ChartDataResult> {
+		const chartData = this.measurePerformance(benchmarkFunctions);
+
+		if (options?.dirPath !== undefined) {
+			await this.chart.createChart(benchMarkNameFile, chartData, options);
+			await saveJsonFile(benchMarkNameFile, chartData, options);
+		}
+		return chartData;
+	}
+
+	/**
+	 * @description Measure the performance of the functions, lower time is better
+	 */
+	measurePerformance<T, U>(
+		benchmarkFunctions: BenchmarkFunctions<T, U>[]
+	): ChartDataResult {
+		const chartData: ChartDataResult = {};
+
+		for (const { name, functionUnderTest, description } of benchmarkFunctions) {
+			performance.mark("start");
+			functionUnderTest();
+			performance.mark("end");
+			performance.measure(name, {
+				start: "start",
+				end: "end",
+				detail: description,
+			});
+
+			const measureResults = performance.getEntriesByName(name);
+			measureResults.forEach((measureResult) => {
+				chartData[name] = {
+					name: measureResult.name,
+					duration: measureResult.duration,
+				};
+			});
+		}
+		return chartData;
+	}
 }
